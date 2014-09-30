202c6899e0723db833ddca97c80be89b76f8c35d
Nittaya <nphonharath@gmail.com>
2014-09-29 18:01:03 -0600

    working

---
+++ contactsApp.js
@@ -0,0 +1,24 @@
+var app = angular.module("contactsApp", []);
+
+app.controller("mainController", 
+	function ($scope) {	
+	// contacts list array
+	$scope.contacts = [];
+
+	// add contact to list
+	$scope.addContact = function(isValid) {
+		if (isValid) {
+			var date = new Date();
+			$scope.contacts.push({
+				dateCreated: date.toDateString(),
+				name: $scope.contactName,
+				email: $scope.contactEmail
+			});
+			
+			// clears input fields after push
+			document.forms["contactForm"].reset();
+			//$scope.contactName = "";
+			//$scope.contactEmail = "";
+		}
+	};
+});
